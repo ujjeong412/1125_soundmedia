e296a41b4e4e02a165eeceb077dad81266f93014
ujjeong412 <117976523+ujjeong412@users.noreply.github.com>
2022-11-25 13:54:57 +0900

    2

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
 
 
-  let mySound;
-  function setup(){
-    soundFormats('mp3', 'ogg');
-    mySound=loadSound('BTS');
-    //mySound.play();
+  var song;
+  var button;
 
+  function setup(){
+    createCanvas(200, 200);
+    song = loadSound('BTS.mp3', loaded);
+    button= createButton('play');
+    button.mousePressed(togglePlaying);
+    background(51);
   }
-  function loadMusic()
-  {
-    mySound.play();
+
+  function togglePlaying(){
+    if(!song.isPlaying()){
+      song.play();
+      song.setVolume(0,3);
+      button.html('pause');
+    }
+    else{
+      song.stop();
+      button.html('play');
+    }
+    
+  }
+
+  function loaded(){
+    console.log('loaded');
   }
