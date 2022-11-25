22c79dfd486d5ef99fc743578deeac2a78d1674f
ujjeong412 <117976523+ujjeong412@users.noreply.github.com>
2022-11-25 13:10:30 +0900

    yeah

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+
+  let mySound;
+  function setup(){
+    soundFormats('mp3', 'ogg');
+    mySound=loadSound('BTS');
+    //mySound.play();
+
+  }
+  function loadMusic()
+  {
+    mySound.play();
+  }
